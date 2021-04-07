cd689f8531cf7cda8a41773a2c608cf41ef3a370
Leo Tan <leotan213@gmail.com>
2021-04-07 06:38:01 -0400

    finally fully done with readme file

---
+++ CSI2110_ProgrammingA2/ProgrammingA2Q1.java
@@ -0,0 +1,321 @@
+import java.lang.Math;
+import java.util.Scanner;
+
+public class ProgrammingA2Q1 {
+
+    /*
+    This method returns the hash value of the given string randWord by calling the helper method partAHelper to do the recursive calls.
+    */
+    public int partA (String randWord, int m){
+        return partAHelper (randWord, m, randWord.length() - 1);
+    }
+
+    /*
+    A helper method for the method partA. This method returns the hash value of the given string randWord.
+    Things I want if I want recurse back:
+    1. 2^m
+    2. remainder operator
+    3. xor expression
+        3.1. left side XOR expression
+            3.1.1. shortened string expression that consists of original string from beginning all the way to right most selected character of the original string (e.g. "Ab" is the shortened string expression and "m" is the right most selected character. The original string expression is "Abm")
+            3.1.2. a method to obtain the shortened string expression
+        3.2 right side XOR expression
+            3.2.1. right most selected character of the original string (e.g. "m" is the right most selected character. The original string expression is "Abm")
+    */
+    private int partAHelper (String randWord, int m, int pos){
+        //System.out.println(randWord);
+        if (randWord.equals("") || pos < 0) {
+            return 0;
+        }
+
+        String shorter = randWord.substring(0, pos);
+        //System.out.println("shorter: " + shorter);
+        int leftXOR = partAHelper (shorter, m, pos - 1) * 33;
+        char c = randWord.charAt(pos);
+        int ascii = (int) c;
+        int rightXOR = ascii;
+
+        //System.out.println("Left XOR: " + String.valueOf(leftXOR));
+        //System.out.println("Right XOR: " + String.valueOf(rightXOR));
+
+        int entireXOR = leftXOR ^ rightXOR;
+        int hashTableSize = (int) Math.pow(2, m);
+        return entireXOR % hashTableSize;
+    }
+
+    /*
+    A helper method for the method partB when n=1. This method returns an array of string that contains the following:
+    First element is an integer for the number of words that has the same hash value as the given number k.
+    Second element is a line by line string of all of the possible words that have the given length n
+    */
+    private String[] oneLoop (int k, int m, String alphabet){
+        int counter = 0;
+        String res = "";
+        int numOfLetters = alphabet.length();
+        for (int loop1 = 0; loop1 < numOfLetters; loop1++) {
+            char c = alphabet.charAt(loop1);
+            String s = Character.toString(c);
+            //String s = String.valueOf(c);
+
+            if ((partA(s, m) == k) && (loop1 == numOfLetters - 1)) {
+                counter++;
+                res += c;
+            }
+            else if ( partA(s, m) == k ) {
+                counter++;
+                res += c + "\n";
+            }
+        }
+        return new String[] {String.valueOf(counter), res};
+    }
+
+    /*
+    A helper method for the method partB when n=2. This method returns an array of string that contains the following:
+    First element is an integer for the number of words that has the same hash value as the given number k.
+    Second element is a line by line string of all of the possible words that have the given length n
+    */
+    private String[] twoLoops (int k, int m, String alphabet){
+        int counter = 0;
+        String res = "";
+        int numOfLetters = alphabet.length();
+        for (int loop1 = 0; loop1 < numOfLetters; loop1++) {
+            for (int loop2 = 0; loop2 < numOfLetters; loop2++) {
+                char c1 = alphabet.charAt(loop1);
+                char c2 = alphabet.charAt(loop2);
+                String s1 = Character.toString(c1);
+                String s2 = Character.toString(c2);
+
+                String s = String.valueOf(s1+s2);
+
+                if ( (partA(s, m) == k) &&
+                    (loop1 == numOfLetters - 1) &&
+                    (loop2 == numOfLetters - 1) ) {
+                        counter++;
+                        res += s;
+                }
+                else if ( partA(s, m) == k ) {
+                    counter++;
+                    res += s + "\n";
+                }
+            }
+        }
+        return new String[] {String.valueOf(counter), res};
+    }
+
+    /*
+    A helper method for the method partB when n=3. This method returns an array of string that contains the following:
+    First element is an integer for the number of words that has the same hash value as the given number k.
+    Second element is a line by line string of all of the possible words that have the given length n
+    */
+    private String[] threeLoops (int k, int m, String alphabet){
+        int counter = 0;
+        String res = "";
+        int numOfLetters = alphabet.length();
+        for (int loop1 = 0; loop1 < numOfLetters; loop1++) {
+            for (int loop2 = 0; loop2 < numOfLetters; loop2++) {
+                for (int loop3 = 0; loop3 < numOfLetters; loop3++) {
+                    char c1 = alphabet.charAt(loop1);
+                    char c2 = alphabet.charAt(loop2);
+                    char c3 = alphabet.charAt(loop3);
+                    String s1 = Character.toString(c1);
+                    String s2 = Character.toString(c2);
+                    String s3 = Character.toString(c3);
+
+                    String s = String.valueOf(s1+s2+s3);
+                    //System.out.println(s);
+
+                    if ( (partA(s, m) == k) &&
+                        (loop1 == numOfLetters - 1) &&
+                        (loop2 == numOfLetters - 1) &&
+                        (loop3 == numOfLetters - 1) ) {
+                            counter++;
+                            res += s;
+                            //System.out.println("Eq string is: " + res);
+                    }
+                    else if ( partA(s, m) == k ) {
+                        counter++;
+                        res += s + "\n";
+                        //System.out.println("Eq string is: " + res);
+                    }
+                }
+            }
+        }
+        return new String[] {String.valueOf(counter), res};
+    }
+
+    /*
+    A helper method for the method partB when n=4. This method returns an array of string that contains the following:
+    First element is an integer for the number of words that has the same hash value as the given number k.
+    Second element is a line by line string of all of the possible words that have the given length n
+    */
+    private String[] fourLoops (int k, int m, String alphabet){
+        int counter = 0;
+        String res = "";
+        int numOfLetters = alphabet.length();
+        for (int loop1 = 0; loop1 < numOfLetters; loop1++) {
+            for (int loop2 = 0; loop2 < numOfLetters; loop2++) {
+                for (int loop3 = 0; loop3 < numOfLetters; loop3++) {
+                    for (int loop4 = 0; loop4 < numOfLetters; loop4++) {
+                        char c1 = alphabet.charAt(loop1);
+                        char c2 = alphabet.charAt(loop2);
+                        char c3 = alphabet.charAt(loop3);
+                        char c4 = alphabet.charAt(loop4);
+                        String s1 = Character.toString(c1);
+                        String s2 = Character.toString(c2);
+                        String s3 = Character.toString(c3);
+                        String s4 = Character.toString(c4);
+
+                        String s = String.valueOf(s1+s2+s3+s4);
+
+                        if ( (partA(s, m) == k) &&
+                            (loop1 == numOfLetters - 1) &&
+                            (loop2 == numOfLetters - 1) &&
+                            (loop3 == numOfLetters - 1) &&
+                            (loop4 == numOfLetters - 1) ) {
+                                counter++;
+                                res += s;
+                        }
+                        else if ( partA(s, m) == k ) {
+                            counter++;
+                            res += s + "\n";
+                        }
+                    }
+                }
+            }
+        }
+        return new String[] {String.valueOf(counter), res};
+    }
+
+    /*
+    A helper method for the method partB when n=5. This method returns an array of string that contains the following:
+    First element is an integer for the number of words that has the same hash value as the given number k.
+    Second element is a line by line string of all of the possible words that have the given length n
+    */
+    private String[] fiveLoops (int k, int m, String alphabet){
+        int counter = 0;
+        String res = "";
+        int numOfLetters = alphabet.length();
+        for (int loop1 = 0; loop1 < numOfLetters; loop1++) {
+            for (int loop2 = 0; loop2 < numOfLetters; loop2++) {
+                for (int loop3 = 0; loop3 < numOfLetters; loop3++) {
+                    for (int loop4 = 0; loop4 < numOfLetters; loop4++) {
+                        for (int loop5 = 0; loop5 < numOfLetters; loop5++) {
+                            char c1 = alphabet.charAt(loop1);
+                            char c2 = alphabet.charAt(loop2);
+                            char c3 = alphabet.charAt(loop3);
+                            char c4 = alphabet.charAt(loop4);
+                            char c5 = alphabet.charAt(loop5);
+                            String s1 = Character.toString(c1);
+                            String s2 = Character.toString(c2);
+                            String s3 = Character.toString(c3);
+                            String s4 = Character.toString(c4);
+                            String s5 = Character.toString(c5);
+
+                            String s = String.valueOf(s1+s2+s3+s4+s5);
+
+                            if ( (partA(s, m) == k) &&
+                                (loop1 == numOfLetters - 1) &&
+                                (loop2 == numOfLetters - 1) &&
+                                (loop3 == numOfLetters - 1) &&
+                                (loop4 == numOfLetters - 1) &&
+                                (loop5 == numOfLetters - 1) ) {
+                                    counter++;
+                                    res += s;
+                            }
+                            else if ( partA(s, m) == k ) {
+                                counter++;
+                                res += s + "\n";
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return new String[] {String.valueOf(counter), res};
+    }
+
+    /*
+    Returns an array of string that contains the following:
+    First element is an integer for the number of words with the given length n
+    Second element is a line by line string of all of the possible words that have the given length n
+    */
+    public String[] partB (int n, int k, int m){
+        String[] result = new String[2];
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        if (n == 1) {
+            result = oneLoop(k, m, alphabet);
+        }
+        else if (n == 2) {
+            result = twoLoops(k, m, alphabet);
+        }
+        else if (n == 3) {
+            result = threeLoops(k, m, alphabet);
+        }
+        else if (n == 4) {
+            result = fourLoops(k, m, alphabet);
+        }
+        else if (n == 5) {
+            result = fiveLoops(k, m, alphabet);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        /*
+        ProgrammingA2Q1 a = new ProgrammingA2Q1 ();
+
+        String[] resultOfPartB = a.partB(3, 10, 12);
+        System.out.println(resultOfPartB[0]);
+        System.out.println(resultOfPartB[1]);
+
+        String[] resultOfPartB2 = a.partB(3, 11, 12);
+        System.out.println(resultOfPartB2[0]);
+        System.out.println(resultOfPartB2[1]);
+
+        String[] resultOfPartB3 = a.partB(4, 25, 10);
+        System.out.println(resultOfPartB3[0]);
+        System.out.println(resultOfPartB3[1]);
+        */
+        /*
+        int test = a.partA("Abm", 10);
+        int test2 = a.partA("fun", 10);
+        int test3 = a.partA("hsq", 10);
+        int test4 = a.partA("pcy", 10);
+        int test5 = a.partA("xsa", 10);
+        System.out.println("Result: " + String.valueOf(test));
+        */
+
+        if (args[0].equals("a") || args[0].equals("A")) {
+            ProgrammingA2Q1 a = new ProgrammingA2Q1 ();
+
+            Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+            System.out.println("Enter hash table size: ");
+            String size = myObj.nextLine();  // Read user input
+            System.out.println("Enter word to hash: ");
+            String word = myObj.nextLine();  // Read user input
+            System.out.println();
+
+            int res = a.partA(word, Integer.parseInt(size));
+            System.out.println();
+            System.out.println( "\nThis is your hashed result: " + String.valueOf(res) );
+
+        }
+        else if (args[0].equals("b") || args[0].equals("B")) {
+            ProgrammingA2Q1 a = new ProgrammingA2Q1 ();
+            Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+            System.out.println("Enter the size of your word from 1 to 5, inclusively: ");
+            String size = myObj.nextLine();  // Read user input
+            System.out.println("Enter the hash value: ");
+            String hashValue = myObj.nextLine();  // Read user input
+            System.out.println("Enter the hash table size from 0 to 25, inclusively: ");
+            String hashTableSize = myObj.nextLine();  // Read user input
+            System.out.println();
+
+            String[] b = a.partB(Integer.parseInt(size), Integer.parseInt(hashValue), Integer.parseInt(hashTableSize));
+            System.out.println();
+            System.out.println("There are " + b[0] + " words with the length " + size + ". They are below:");
+            System.out.println(b[1]);
+        }
+    }
+}
---
+++ CSI2110_ProgrammingA2/README.txt
@@ -0,0 +1,20 @@
+CSI2110 Programming Assignment 2
+Student name: Leo Tan
+Student number: 300018447
+Include files: README.txt, Constants.java, Logic.java, ProgrammingA2Q1.java, ProgrammingA2Q2.java, ProgrammingA2Q3.java, q3test.txt, and Task 1 Part C.pdf
+
+Student comments:
+Please compile ALL of the java files in the given folder before running them.
+
+When you run the file ProgrammingA2Q1.java, put either "a" or "A" in the command arguments to test for Task 1 Part A, or 
+put either "b" or "B" in the command arguments to test for Task 1 Part B. Just follow the instructions after putting in "a", "A", "b", or "B".
+E.g. java ProgrammingA2Q1 "a" or java ProgrammingA2Q1 "b".
+
+To check my solution for Task 1 Part C, go to the given pdf called "Task 1 Part C.pdf".
+
+When you run the file ProgrammingA2Q2.java, enclose the message or text that has to be tested in quotes in the command arguments. 
+E.g. java ProgrammingA2Q2 "data structure is fun"
+
+Before you run the file ProgrammingA2Q3.java, erase all of the contents in q3test.txt, and type in the sentences that will be tested in that text file. 
+Save the text file, and run the command (without the quotes) "java ProgrammingA2Q3 q3test.txt" as the java file will read the contents from that text 
+file and produce a hash result.
